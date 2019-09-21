494eb6359279c32ba5ca9349c1a920d813990559
Arjen van Bochoven <nejra@me.com>
2019-09-21 12:21:17 +0200

    Add detail_widget

---
+++ views/storage_detail_widget.php
@@ -0,0 +1,17 @@
+<div class="col-lg-4">
+    <h4><i class="fa fa-hdd-o"></i> <span data-i18n="disk_report.storage"></span></h4>
+    <table>
+        <tr>
+            <th data-i18n="disk_report.size"></th><td class="mr-TotalSize"></td>
+        </tr>
+        <tr>
+            <th data-i18n="disk_report.used"></th><td class="mr-UsedSize"></td>
+        </tr>
+        <tr>
+            <th data-i18n="disk_report.free"></th><td class="mr-freespace"></td>
+        </tr>
+        <tr>
+            <th data-i18n="disk_report.smartstatus"></th><td class="mr-smartstatus"></td>
+        </tr>
+    </table>
+</div>
