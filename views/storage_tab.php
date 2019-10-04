2a8db9fc2bc5da34bc7d4718659ac589691c1f3c
Arjen van Bochoven <nejra@me.com>
2019-10-04 09:31:12 +0200

    Move detail widget logic to tab

--- views/storage_tab.php
+++ views/storage_tab.php
@@ -14,126 +14,155 @@ $(document).on('appReady', function(e, lang) {
  */
 var drawStoragePlots = function(serialNumber, divid) {
 
-// Get storage data
-var url = appUrl + '/module/disk_report/get_data/'+serialNumber
-var chart;
-d3.json(url, function(err, data){
+    // Get storage data
+    var url = appUrl + '/module/disk_report/get_data/'+serialNumber
+    var chart;
+    d3.json(url, function(err, data){
 
-    var height = 300;
-    var width = 250;
-    var siFormat = d3.format('0.2s');
-    var format = function(d){ return siFormat(d) + 'B'}
+        var height = 300;
+        var width = 250;
+        var siFormat = d3.format('0.2s');
+        var format = function(d){ return siFormat(d) + 'B'}
 
-    $.each(data, function(index, obj){
+        $.each(data, function(index, obj){
 
-        var id = 'storage-plot-'+index
-        var row = d3.select("#"+divid)
-            .append('div')
-            .attr('class', 'row')
+            var id = 'storage-plot-'+index
+            var row = d3.select("#"+divid)
+                .append('div')
+                .attr('class', 'row')
 
-        d3.select("#"+divid).append('hr')
+            d3.select("#"+divid).append('hr')
 
-        var info = row.append('div')
-                        .attr('class', 'col-sm-6')
+            var info = row.append('div')
+                            .attr('class', 'col-sm-6')
 
-        info.append('h4')
-            .text(obj.volumename)
+            info.append('h4')
+                .text(obj.volumename)
 
-        var table = info.append('table')
-                        .attr('class', 'table table-striped')
-                        .append('tbody')
+            var table = info.append('table')
+                            .attr('class', 'table table-striped')
+                            .append('tbody')
 
-        // get encryption string
-        var encrypted = obj.encrypted == 1 ? i18n.t('disk_report.encrypted') : i18n.t('disk_report.not_encrypted');
-        if(obj.encrypted == -1)
-        {
-            encrypted = i18n.t('unknown')
-        }
-
-        var props = [
+            // get encryption string
+            var encrypted = obj.encrypted == 1 ? i18n.t('disk_report.encrypted') : i18n.t('disk_report.not_encrypted');
+            if(obj.encrypted == -1)
             {
-                key: i18n.t('disk_report.mountpoint'),
-                val: obj.mountpoint
-            },
-            {
-                key: i18n.t('disk_report.media_type'),
-                val: obj.media_type && obj.media_type.toUpperCase()
-            },
-            {
-                key: i18n.t('disk_report.volume_type'),
-                val: obj.volumetype.toUpperCase()
-            },
-            {
-                key: i18n.t('disk_report.smartstatus'),
-                val: obj.smartstatus
-            },{
-                key: i18n.t('disk_report.bus_protocol'),
-                val: obj.busprotocol
-            },{
-                key: i18n.t('disk_report.type'),
-                val: obj.internal == 1 ? i18n.t('disk_report.internal') : i18n.t('disk_report.external')
-            },
-            {
-                key: i18n.t('disk_report.encryption_status'),
-                val: encrypted
-            }];
+                encrypted = i18n.t('unknown')
+            }
 
-        var tr = table.selectAll("tr")
-                        .data(props)
-                        .enter().append("tr")
-                        .html(function(d) { return '<th>'+d.key+'</th><td>'+d.val+'</td>'; })
+            var props = [
+                {
+                    key: i18n.t('disk_report.mountpoint'),
+                    val: obj.mountpoint
+                },
+                {
+                    key: i18n.t('disk_report.media_type'),
+                    val: obj.media_type && obj.media_type.toUpperCase()
+                },
+                {
+                    key: i18n.t('disk_report.volume_type'),
+                    val: obj.volumetype.toUpperCase()
+                },
+                {
+                    key: i18n.t('disk_report.smartstatus'),
+                    val: obj.smartstatus
+                },{
+                    key: i18n.t('disk_report.bus_protocol'),
+                    val: obj.busprotocol
+                },{
+                    key: i18n.t('disk_report.type'),
+                    val: obj.internal == 1 ? i18n.t('disk_report.internal') : i18n.t('disk_report.external')
+                },
+                {
+                    key: i18n.t('disk_report.encryption_status'),
+                    val: encrypted
+                }
+            ];
+
+            // Populate detail widget with stats from /
+            if (obj.mountpoint == '/') {
+                var bootprops = [
+                    {
+                        key: i18n.t('disk_report.size'),
+                        val: fileSize(obj.totalsize, 1)
+                    },
+                    {
+                        key: i18n.t('disk_report.used'),
+                        val: fileSize(obj.totalsize - obj.freespace, 1)
+                    },
+                    {
+                        key: i18n.t('disk_report.free'),
+                        val: fileSize(obj.freespace, 1)
+                    },
+                    {
+                        key: i18n.t('disk_report.smartstatus'),
+                        val: obj.smartstatus
+                    },
+                    {
+                        key: i18n.t('disk_report.encryption_status'),
+                        val: encrypted
+                    }
+                ];
+                d3.select('#disk_report_detail')
+                    .append("tbody")
+                    .selectAll("tr")
+                    .data(bootprops)
+                    .enter().append("tr")
+                    .html(function(d) { return '<th>'+d.key+'</th><td>'+d.val+'</td>'; })
+            }
+
+            var tr = table.selectAll("tr")
+                            .data(props)
+                            .enter().append("tr")
+                            .html(function(d) { return '<th>'+d.key+'</th><td>'+d.val+'</td>'; })
+
+            row.append('div')
+                    .attr('class', 'col-sm-6')
+                        .append('svg')
+                        .attr('id', id)
+                        .attr('class', 'pull-right')
 
 
-        row.append('div')
-                .attr('class', 'col-sm-6')
-                    .append('svg')
-                    .attr('id', id)
-                    .attr('class', 'pull-right')
+            // Filter data
+            var fill = [
+                {
+                    key: i18n.t('disk_report.used'),
+                    cnt: obj.totalsize - obj.freespace
+                },
+                {
+                    key: i18n.t('disk_report.free'),
+                    cnt:obj.freespace
+                }
+            ];
 
+            nv.addGraph(function() {
+                var chart = nv.models.pieChart()
+                    .x(function(d) { return d.key })
+                    .y(function(d) { return d.cnt })
+                    .showLabels(true)
+                    .valueFormat(format)
+                    .donut(true)
+                    .labelsOutside(true)
+                    .labelType('value');
 
-           // Filter data
-           var fill = [
-               {
-                   key: i18n.t('disk_report.used'),
-                   cnt: obj.totalsize - obj.freespace
-               },
-            {
-                key: i18n.t('disk_report.free'),
-                cnt:obj.freespace
-            }];
+                chart.title(format(obj.totalsize));
 
-        nv.addGraph(function() {
-            var chart = nv.models.pieChart()
-                .x(function(d) { return d.key })
-                .y(function(d) { return d.cnt })
-                .showLabels(true)
-                .valueFormat(format)
-                .donut(true)
-                .labelsOutside(true)
-                .labelType('value');
+                //chart.pie.donut(true);
 
-            chart.title(format(obj.totalsize));
+                d3.select("#"+id)
+                    .style('height', height)
+                    .style('width', width)
+                    .datum(fill)
+                    .transition().duration(1200)
+                    .call(chart);
 
-            //chart.pie.donut(true);
+                // Remove fill from labels
+                d3.selectAll('#'+id+' .nv-pieLabels text').style('fill', null);
 
-            d3.select("#"+id)
-                .style('height', height)
-                .style('width', width)
-                .datum(fill)
-                .transition().duration(1200)
-                .call(chart);
+                return chart;
 
-            // Remove fill from labels
-            d3.selectAll('#'+id+' .nv-pieLabels text').style('fill', null);
-
-            return chart;
-
+            });
         });
-
     });
-
-
-
-});
 };
 </script>
