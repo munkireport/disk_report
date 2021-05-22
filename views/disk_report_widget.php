08eb71d357366de91d9439648d14d9fbb12a429b
Mosen <mosen@users.noreply.github.com>
2021-05-22 17:20:18 +1000

    Bootstrap 4 Style Updates for MunkiReport-PHP v6.0.0 beta 1 Start using version numbers in dependent packages. Include basic GraphQL schema file for future GraphQL support

--- views/disk_report_widget.php
+++ views/disk_report_widget.php
@@ -1,17 +1,17 @@
 		<div class="col-lg-4 col-md-6">
 
-			<div class="panel panel-default">
+			<div class="card">
 
-				<div class="panel-heading">
+				<div class="card-header">
 
-					<h3 class="panel-title"><i class="fa fa-hdd-o"></i>
+					<i class="fa fa-hdd-o"></i>
 					    <span data-i18n="free_disk_space"></span>
-					    <list-link data-url="/show/listing/disk_report/disk"></list-link>
-					</h3>
+					    <a href="/show/listing/disk_report/disk" class="pull-right"><i class="fa fa-list"></i></a>
+					
 
 				</div>
 
-				<div class="panel-body text-center">
+				<div class="card-body text-center">
 
 
 					<a id="disk-danger" class="btn btn-danger hide">
